04cb1ab7143f46d053d27f99086d1960c8e29a37
Javier Gil <javier.gil@module-7.com>
2016-10-26 09:03:38 +0200

    Reformatted the javascript files

--- Resources/public/js/m7-searchable-table.js
+++ Resources/public/js/m7-searchable-table.js
@@ -1,92 +1,93 @@
-;(function($, window, document, undefined) {
-    var pluginName = 'm7SearchableTable', dataKey = 'plugin_' + pluginName;
-    
+;
+(function($, window, document, undefined) {
+    var pluginName = 'm7SearchableTable',
+        dataKey = 'plugin_' + pluginName;
+
     var Plugin = function(element, options) {
-    	this.element = element;
-    	this.options = {
-    		pagination: true,
-    		itemsPerPage: 15
-    	};
-    	
-    	this.init(options);
+        this.element = element;
+        this.options = {
+            pagination: true,
+            itemsPerPage: 15
+        };
+
+        this.init(options);
     };
-    
+
     Plugin.prototype = {
-    	init : function(options) {
-    	    $.extend(this.options, options);
-    	    var plugin = this;
-    	    var $element = $(this.element);
-    	    
-    	    this.searchFields = $element.attr("data-terms").split(',');
-    	    
-    	    var listOptions = {
-    	    	valueNames: this.searchFields,
-    	    	page: this.options.itemsPerPage
-    	    };
-    	    
-    	    if (this._loadPagination()) {
-    	    	listOptions.plugins = [ ListPagination({
-    	    		outerWindow : 1
-    	    	}) ];
-    	    }
-    	    this.list = new List(this.element, listOptions);
-    	    this.list.on('updated', function(){
-    	    	plugin.options.onUpdate(plugin.element);
-    	    });
-    	},
-    	
-    	_loadPagination : function() {
-    		if (this.options.pagination) {
-    			var list = $(this.element).find('.list').first();
-    			var items = list.children();
-    			
-    			return items.length > this.options.itemsPerPage;
-    		}
-    		else {
-    			return false;
-    		}
-    	}
+        init: function(options) {
+            $.extend(this.options, options);
+            var plugin = this;
+            var $element = $(this.element);
+
+            this.searchFields = $element.attr("data-terms").split(',');
+
+            var listOptions = {
+                valueNames: this.searchFields,
+                page: this.options.itemsPerPage
+            };
+
+            if (this._loadPagination()) {
+                listOptions.plugins = [ListPagination({
+                    outerWindow: 1
+                })];
+            }
+            this.list = new List(this.element, listOptions);
+            this.list.on('updated', function() {
+                plugin.options.onUpdate(plugin.element);
+            });
+        },
+
+        _loadPagination: function() {
+            if (this.options.pagination) {
+                var list = $(this.element).find('.list').first();
+                var items = list.children();
+
+                return items.length > this.options.itemsPerPage;
+            } else {
+                return false;
+            }
+        }
     };
-    
+
     $.fn[pluginName] = function(options) {
-		var args = arguments;
-	
-		if (options === undefined || typeof options === 'object') {
-		    // Creates a new plugin instance, for each selected element, and
-		    // stores a reference withint the element's data
-		    return this.each(function() {
-			if (!$.data(this, 'plugin_' + pluginName)) {
-			    $.data(this, 'plugin_' + pluginName, new Plugin(this,
-				    options));
-			}
-		    });
-		} else if (typeof options === 'string' && options[0] !== '_'
-			&& options !== 'init') {
-		    // Call a public pluguin method (not starting with an
-		    // underscore) for each
-		    // selected element.
-		    if (Array.prototype.slice.call(args, 1).length == 0
-			    && $.inArray(options, $.fn[pluginName].getters) != -1) {
-			// If the user does not pass any arguments and the method
-			// allows to
-			// work as a getter then break the chainability so we can
-			// return a value
-			// instead the element reference.
-			var instance = $.data(this[0], 'plugin_' + pluginName);
-			return instance[options].apply(instance, Array.prototype.slice
-				.call(args, 1));
-		    } else {
-			// Invoke the speficied method on each selected element
-			return this.each(function() {
-			    var instance = $.data(this, 'plugin_' + pluginName);
-			    if (instance instanceof Plugin
-				    && typeof instance[options] === 'function') {
-				instance[options].apply(instance, Array.prototype.slice
-					.call(args, 1));
-			    }
-			});
-		    }
-		}
+        var args = arguments;
+
+        if (options === undefined || typeof options === 'object') {
+            // Creates a new plugin instance, for each selected element, and
+            // stores a reference withint the element's data
+            return this.each(function() {
+                if (!$.data(this, 'plugin_' + pluginName)) {
+                    $.data(this, 'plugin_' + pluginName, new Plugin(this,
+                        options));
+                }
+            });
+        } else if (typeof options === 'string' && options[0] !== '_' &&
+            options !== 'init') {
+            // Call a public pluguin method (not starting with an
+            // underscore) for each
+            // selected element.
+            if (Array.prototype.slice.call(args, 1).length == 0 &&
+                $.inArray(options, $.fn[pluginName].getters) != -1) {
+                // If the user does not pass any arguments and the method
+                // allows to
+                // work as a getter then break the chainability so we can
+                // return a value
+                // instead the element reference.
+                var instance = $.data(this[0], 'plugin_' + pluginName);
+                return instance[options].apply(instance, Array.prototype.slice
+                    .call(args, 1));
+            } else {
+                // Invoke the speficied method on each selected element
+                return this.each(function() {
+                    var instance = $.data(this, 'plugin_' + pluginName);
+                    if (instance instanceof Plugin &&
+                        typeof instance[options] === 'function') {
+                        instance[options].apply(instance, Array.prototype.slice
+                            .call(args, 1));
+                    }
+                });
+            }
+        }
     };
-    
+
 })(jQuery, window, document);
